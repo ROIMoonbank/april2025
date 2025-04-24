c0fb99b11d5932de6e6d03da450ce7403a33f6e9
Grant Moyle <Grant.A.Moyle@roitraining.com>
2025-04-24 06:06:55 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from waitress import serve
+import os
+import logging
+import random
+
+app = Flask(__name__)
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.DEBUG)
+
+@app.route("/")
+def index():
+  randomnum = random.randint(1, 100000)/100
+  return "Your Bank Account Balance is: $" + str(randomnum) + "!\n"
+
+@app.route("/version")
+def version():
+  return "ROI Moonbank Demo 1.0\n"
+
+@app.route("/hello")
+def hello():
+  return "Hello - welcome to Moonbank\n"
+
+if __name__ == "__main__":
+  serve(app,host="0.0.0.0",port=int(os.environ.get("PORT", 8080)))
