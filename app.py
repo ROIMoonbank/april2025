2b0488b4a2556eb65aefaa5d164250b46ebe0ea6
Grant Moyle <Grant.A.Moyle@roitraining.com>
2025-04-24 06:35:02 -0500

    added listfiles

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from waitress import serve
 import os
 import logging
 import random
+from google.cloud import storage
 
 app = Flask(__name__)
 logger = logging.getLogger('waitress')
@@ -13,6 +14,20 @@ def index():
   randomnum = random.randint(1, 100000)/100
   return "Your Bank Account Balance is: $" + str(randomnum) + "!\n"
 
+@app.route("/listfiles")
+def listfiles():
+  # get a list of files in the gcs bucket gs://roi-mb-feb2025-emea-bucket
+  returnmessage=""
+  bucket_name="roi-mb-feb2025-emea-bucket"
+  storage_client = storage.Client()
+  bucket = storage_client.bucket(bucket_name)
+  print(f"Successfully connected to bucket: gs://{bucket_name}")
+  blobs = bucket.list_blobs() # You can also specify a prefix: bucket.list_blobs(prefix="some/folder/")
+  count = 0
+  for blob in blobs:
+      returnmessage=returnmessage+f"- {blob.name} (Size: {blob.size} bytes)\n"           
+  return returnmessage
+  
 @app.route("/version")
 def version():
   return "ROI Moonbank Demo 1.0\n"
